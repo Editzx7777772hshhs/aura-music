072a7aa53a8db2f93730e3147a598f78356de390
Editzx7777772hshhs <igknight778@gmail.com>
2026-08-19 15:54:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
+      }
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -12,13 +24,7 @@ export default defineConfig({
     extensions: [".mjs", ".js", ".ts", ".jsx", ".tsx", ".json"]
   },
   server: {
-    port: 5173,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_DEV_API_PROXY_TARGET || "http://localhost:3000",
-        changeOrigin: true
-      }
-    }
+    port: 5173
   },
   build: {
     outDir: "dist",
