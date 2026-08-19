0650f30cebaa74d6f08df28d1b39f1a3afbf3df1
Editzx7777772hshhs <igknight778@gmail.com>
2026-08-19 16:11:05 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
-      }
-    })
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
