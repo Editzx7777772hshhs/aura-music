450b0b8ef8f1fd0ebca1a0b188079e2f2816e01d
Editzx7777772hshhs <igknight778@gmail.com>
2026-08-19 20:55:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect, useRef } from "react";
 import Sidebar from "./components/layout/Sidebar";
 import Topbar from "./components/layout/Topbar";
 import MobileNav from "./components/layout/MobileNav";
