64eaad9eab0c9292e8fff9649fa757efb7fbe517
Editzx7777772hshhs <igknight778@gmail.com>
2026-08-19 21:33:57 +0530

    Update vite.config.js
    .

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    extensions: [".mjs", ".js", ".mts", ".ts", ".jsx", ".tsx", ".json"],
-  },
-  build: {
-    outDir: "dist",
-  },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  }
 });
 
